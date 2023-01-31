3dea605fdc63c158a827c26f789cc3bb3e240061
Rubens Rojas <rubensarojas@gmail.com>
2023-01-31 00:56:29 -0300

    Get contacts and get individual contact endpoint and controller

---
+++ backend/src/controllers/GetContactController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import prisma from '../database/prismaClient';
+
+export default class GetContactController {
+  async many(request: Request, response: Response) {
+
+    try {
+      const contacts = await prisma.contact.findMany();
+
+      return response.status(201).json(contacts);
+    } catch (e) {
+      return response.status(400).json({ error: e })
+    }
+  }
+
+  async unique(request: Request, response: Response) {
+    const { id } = request.params;
+
+    try {
+      const contact = await prisma.contact.findUnique({
+        where: { id: Number(id) }
+      });
+
+      return response.status(201).json(contact);
+    } catch (e) {
+      return response.status(404).json({ error: `Could not find contact id: ${id}` })
+    }
+  }
+};
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,16 +1,20 @@
 import { Router } from 'express';
 import CreateContactController from './controllers/CreateContactController';
 import DeleteContactController from './controllers/DeleteContactController';
+import GetContactController from './controllers/GetContactController';
 import UpdateContactController from './controllers/UpdateContactController';
 
 const router = Router();
 
 // Controllers
+const getContact = new GetContactController();
 const createProduct = new CreateContactController();
 const deleteProduct = new DeleteContactController();
 const updateProduct = new UpdateContactController();
 
 // Routes
+router.get('/contacts', getContact.many);
+router.get('/contacts/:id', getContact.unique);
 router.post('/contacts', createProduct.handle);
 router.delete('/contacts', deleteProduct.handle);
 router.patch('/contacts', updateProduct.handle);
