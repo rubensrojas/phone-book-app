60370f85d88a8dc9a559c8f4b33f0e97fe6abbd1
Rubens Rojas <rubensarojas@gmail.com>
2023-01-31 00:50:04 -0300

    Update Contact route and controller

---
+++ backend/src/controllers/UpdateContactController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import prisma from '../database/prismaClient';
+
+export default class UpdateContactController {
+  async handle(request: Request, response: Response) {
+
+    const { id, firstName, lastName, phoneNumber } = request.body;
+
+    try {
+      const contact = await prisma.contact.update({
+        where: { id },
+        data: { firstName, lastName, phoneNumber }
+      })
+
+      return response.status(201).json(contact);
+    } catch (e) {
+      return response.status(404).json({ error: `Did not found contact id: ${id}.` })
+    }
+  }
+};
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 import CreateContactController from './controllers/CreateContactController';
 import DeleteContactController from './controllers/DeleteContactController';
+import UpdateContactController from './controllers/UpdateContactController';
 
 const router = Router();
 
 // Controllers
 const createProduct = new CreateContactController();
 const deleteProduct = new DeleteContactController();
+const updateProduct = new UpdateContactController();
 
 // Routes
 router.post('/contacts', createProduct.handle);
 router.delete('/contacts', deleteProduct.handle);
+router.patch('/contacts', updateProduct.handle);
 
 
 export { router };
