8fb1f401a1c517a7309836e34623664fbdb85a4f
Rubens Rojas <rubensarojas@gmail.com>
2023-01-31 00:43:19 -0300

    Delete Contact route and controller

---
+++ backend/src/controllers/DeleteContactController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import prisma from '../database/prismaClient';
+
+export default class DeleteContactController {
+  async handle(request: Request, response: Response) {
+    const { id } = request.body;
+
+    try {
+      const contact = await prisma.contact.delete({
+        where: { id }
+      })
+
+      return response.status(204).json(contact);
+    } catch (e) {
+      return response.status(404).json({ error: `Did not found contact id: ${id}.` })
+    }
+  }
+};
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import CreateContactController from './controllers/CreateContactController';
+import DeleteContactController from './controllers/DeleteContactController';
 
 const router = Router();
 
 // Controllers
 const createProduct = new CreateContactController();
+const deleteProduct = new DeleteContactController();
 
 // Routes
 router.post('/contacts', createProduct.handle);
+router.delete('/contacts', deleteProduct.handle);
 
 
 export { router };
